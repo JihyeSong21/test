96f2a7f42aff6442739da85d08e2a058a51878b6
JihyeSong21 <thddoden1004@naver.com>
2020-12-23 17:19:03 +0900

    drinkk

---
+++ 고급 자판기/src/Drink/Drink.java
@@ -0,0 +1,16 @@
+package Drink;
+
+public class Drink {
+	String name;
+	int price;
+	int volume;
+
+	public Drink() {
+	}
+
+	public Drink(String name, int price, int volume) {
+		this.name = name;
+		this.price = price;
+		this.volume = volume;
+	}
+}
